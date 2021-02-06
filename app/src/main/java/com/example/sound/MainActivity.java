264fcf99c088a0c0cfedb2c1800d9cb35a312a63
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-06 16:43:32 +0900

    ここまで

--- app/src/main/java/com/example/sound/MainActivity.java
+++ app/src/main/java/com/example/sound/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.sound;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View btnSeikai = findViewById(R.id.maru);
+        btnSeikai.setOnTouchListener(this);
+
+        View btnFuseikai = findViewById(R.id.batu);
+        btnFuseikai.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
 }
